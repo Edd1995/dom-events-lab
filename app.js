68e9148ce6d7fc963dbe9287763f55f6cb1ac721
edevelopk20@gmail.com <edwardh1995@yahoo.com>
2024-03-26 22:28:43 -0400

    works for 1 interger

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const numbers = document.querySelectorAll(".button")
 
 /*-------------------------------- Variables --------------------------------*/
 
+let numberOne = null;
+let numberTwo = null;
+let operator = null;
+
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -11,9 +15,31 @@ const numbers = document.querySelectorAll(".button")
 numbers.forEach(number => {
   number.addEventListener("click", (event) => {
     // This log is for testing purposes to verify we're getting the correct value
-    console.log(event.target.innerText);
-    // Future logic to capture the button's value goes here...
+      // console.log(event.target.innerText);
+      
+      let pressed = event.target.innerText
+
+      // Future logic to capture the button's value goes here...)
+      if (numberOne === null && !operator && !isNaN(pressed)) { 
+          numberOne = pressed
+      }
+       if (numberOne !== null && !operator && (pressed === '/' || pressed === '*' || pressed === '-' || pressed ==='+'  )) { 
+           operator = pressed;
+       }
+       if (numberOne !== null && operator && !isNaN(pressed)) { 
+          numberTwo = pressed
+       }
+      if (numberOne !== null && numberTwo !== null && operator !== null) {
+          doMath(numberOne, numberTwo, operator);
+       }
   });
 });
 
 /*-------------------------------- Functions --------------------------------*/
+
+const doMath = (num1, num2, operator) => { 
+
+    let total = eval(`${num1} ${operator} ${num2}`);
+    console.log(`${num1} ${operator} ${num2} = ${total}`)
+
+}
