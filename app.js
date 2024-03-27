cc08c5e81cab9f80e531858966d052931622177e
edevelopk20@gmail.com <edwardh1995@yahoo.com>
2024-03-26 22:53:12 -0400

    added clear

--- app.js
+++ app.js
@@ -13,33 +13,45 @@ let operator = null;
 /*----------------------------- Event Listeners -----------------------------*/
 
 numbers.forEach(number => {
-  number.addEventListener("click", (event) => {
-    // This log is for testing purposes to verify we're getting the correct value
-      // console.log(event.target.innerText);
+    number.addEventListener("click", (event) => {
+        // This log is for testing purposes to verify we're getting the correct value
+        // console.log(event.target.innerText);
       
-      let pressed = event.target.innerText
+        let pressed = event.target.innerText
 
-      // Future logic to capture the button's value goes here...)
-      if (numberOne === null && !operator && !isNaN(pressed)) { 
-          numberOne = pressed
-      }
-       if (numberOne !== null && !operator && (pressed === '/' || pressed === '*' || pressed === '-' || pressed ==='+'  )) { 
-           operator = pressed;
-       }
-       if (numberOne !== null && operator && !isNaN(pressed)) { 
-          numberTwo = pressed
-       }
-      if (numberOne !== null && numberTwo !== null && operator !== null) {
-          doMath(numberOne, numberTwo, operator);
-       }
-  });
+        // Future logic to capture the button's value goes here...)
+        if (pressed === 'C') {
+            numberOne = null;
+            numberTwo = null;
+            operator = null;
+            console.log('RESET')
+        }
+        else if (pressed !== 'C') {
+      
+            if (numberOne === null && !operator) {
+                numberOne = pressed;
+            }
+          
+            if (numberOne !== null && !operator && (pressed === '/' || pressed === '*' || pressed === '-' || pressed === '+')) {
+                operator = pressed;
+            }
+            if (numberOne !== null && operator) {
+                numberTwo = pressed
+            }
+            if (numberOne !== null && numberTwo !== null && operator !== null) {
+                doMath(numberOne, numberTwo, operator);
+            }
+        }
+    })
+
+
 });
 
 /*-------------------------------- Functions --------------------------------*/
 
 const doMath = (num1, num2, operator) => { 
 
-    let total = eval(`${num1} ${operator} ${num2}`);
+    let total = eval(`${num1}${operator}${num2}`);
     console.log(`${num1} ${operator} ${num2} = ${total}`)
 
 }
